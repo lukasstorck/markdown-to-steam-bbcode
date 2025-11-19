a49aa551240c951988d56b8e022ca04eef344caf
Lukas <38840142+lukasstorck@users.noreply.github.com>
2025-11-19 23:15:10 +0100

    feat: use .join("\n") for more readable test cases, fix some tests

--- src/tests.js
+++ src/tests.js
@@ -2,123 +2,121 @@
 const testCases = [
   {
     name: "Headings",
-    input: `# Heading 1
-## Heading 2
-### Heading 3
-`,
-    expected: `[h1]Heading 1[/h1]
-[h2]Heading 2[/h2]
-[h3]Heading 3[/h3]
-`,
+    input: ["# Heading 1", "## Heading 2", "### Heading 3", ""].join("\n"),
+    expected: [
+      "[h1]Heading 1[/h1]",
+      "[h2]Heading 2[/h2]",
+      "[h3]Heading 3[/h3]",
+      "",
+    ].join("\n"),
   },
   {
     name: "Bold and Italic",
-    input: `**Bold**
-*Italic*
-_Italic_
-~~Strike~~
-`,
-    expected: `[b]Bold[/b]
-[i]Italic[/i]
-[i]Italic[/i]
-[strike]Strike[/strike]
-`,
+    input: ["**Bold**", "*Italic*", "_Italic_", "~~Strike~~"].join("\n"),
+    expected: [
+      "[b]Bold[/b]",
+      "[i]Italic[/i]",
+      "[i]Italic[/i]",
+      "[strike]Strike[/strike]",
+    ].join("\n"),
   },
   {
     name: "Links",
-    input: `[Google](https://google.com)`,
-    expected: `[url=https://google.com]Google[/url]
-`,
+    input: "[Google](https://google.com)",
+    expected: ["[url=https://google.com]Google[/url]", ""].join("\n"),
   },
   {
     name: "Quotes",
-    input: `> This is a quote`,
-    expected: `[quote]This is a quote[/quote]
-`,
+    input: "> This is a quote",
+    expected: ["[quote]This is a quote[/quote]", ""].join("\n"),
   },
   {
     name: "Inline and Block Code",
-    input: `\`inline code\`
-
-\`\`\`
-multiline
-code block
-\`\`\`
-`,
-    expected: `[code]inline code[/code]
-
-[code]multiline
-code block[/code]`,
+    input: ["`inline code`", "", "```", "multiline", "code block", "```"].join(
+      "\n"
+    ),
+    expected: [
+      "[code]inline code[/code]",
+      "",
+      "[code]multiline",
+      "code block[/code]",
+    ].join("\n"),
   },
   {
     name: "Unordered List",
-    input: `* Item 1
-  * Item 1.1
-    * Item 1.1.1
-* Item 2
-- Dash item
-+ Plus item
-`,
-    expected: `[list]
-  [*] Item 1
-  [list]
-    [*] Item 1.1
-    [list]
-      [*] Item 1.1.1
-    [/list]
-  [/list]
-  [*] Item 2
-  [*] Dash item
-  [*] Plus item
-[/list]
-`,
+    input: [
+      "* Item 1",
+      "  * Item 1.1",
+      "    * Item 1.1.1",
+      "* Item 2",
+      "- Dash item",
+      "+ Plus item",
+    ].join("\n"),
+    expected: [
+      "[list]",
+      "  [*] Item 1",
+      "  [list]",
+      "    [*] Item 1.1",
+      "    [list]",
+      "      [*] Item 1.1.1",
+      "    [/list]",
+      "  [/list]",
+      "  [*] Item 2",
+      "  [*] Dash item",
+      "  [*] Plus item",
+      "[/list]",
+    ].join("\n"),
   },
   {
     name: "Ordered List",
-    input: `1. Step 1
-   1. Step 1.1
-      1. Step 1.1.1
-2. Step 2
-`,
-    expected: `[olist]
-  [*] Step 1
-  [olist]
-    [*] Step 1.1
-    [olist]
-      [*] Step 1.1.1
-    [/olist]
-  [/olist]
-  [*] Step 2
-[/olist]
-`,
+    input: [
+      "1. Step 1",
+      "   1. Step 1.1",
+      "      1. Step 1.1.1",
+      "2. Step 2",
+    ].join("\n"),
+    expected: [
+      "[olist]",
+      "  [*] Step 1",
+      "  [olist]",
+      "    [*] Step 1.1",
+      "    [olist]",
+      "      [*] Step 1.1.1",
+      "    [/olist]",
+      "  [/olist]",
+      "  [*] Step 2",
+      "[/olist]",
+    ].join("\n"),
   },
   {
     name: "Table",
-    input: `| Name | Age |
-|------|-----|
-| John | 65  |
-| Gitte| 40  |
-| Sussie | 19 |
-`,
-    expected: `[table]
-  [tr]
-    [th]Name[/th]
-    [th]Age[/th]
-  [/tr]
-  [tr]
-    [td]John[/td]
-    [td]65[/td]
-  [/tr]
-  [tr]
-    [td]Gitte[/td]
-    [td]40[/td]
-  [/tr]
-  [tr]
-    [td]Sussie[/td]
-    [td]19[/td]
-  [/tr]
-[/table]
-`,
+    input: [
+      "| Name   | Age |",
+      "| ------ | --- |",
+      "| John   | 65  |",
+      "| Gitte  | 40  |",
+      "| Sussie | 19  |",
+    ].join("\n"),
+    expected: [
+      "[table]",
+      "  [tr]",
+      "    [th]Name[/th]",
+      "    [th]Age[/th]",
+      "  [/tr]",
+      "  [tr]",
+      "    [td]John[/td]",
+      "    [td]65[/td]",
+      "  [/tr]",
+      "  [tr]",
+      "    [td]Gitte[/td]",
+      "    [td]40[/td]",
+      "  [/tr]",
+      "  [tr]",
+      "    [td]Sussie[/td]",
+      "    [td]19[/td]",
+      "  [/tr]",
+      "[/table]",
+    ].join("\n"),
   },
 ];
 
